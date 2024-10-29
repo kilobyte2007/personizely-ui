f3c8dfdecc360d638889147f1d49a7397b550b57
Sergiu Cazac <kilobyte2007@gmail.com>
2024-10-29 22:06:56 +0200

    Prevent selection for color handles

--- src/components/ui/color-picker/Handle.vue
+++ src/components/ui/color-picker/Handle.vue
@@ -1,6 +1,6 @@
 <template>
   <div
-    :class="cn('absolute rounded-md cursor-pointer border-2 border-gray-300 shadow-sm scale-[1.2] ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2', $attrs.class)"
+    :class="cn('absolute rounded-md cursor-pointer select-none border-2 border-gray-300 shadow-sm scale-[1.2] ring-offset-background focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2', $attrs.class)"
     role="presentation"
     tabindex="0"
     @keydown.up.prevent="$emit('up', $event)"
